81a633067b392e92aaf372665f923c1e7c978ca2
hharryyf <harryheyifan99@gmail.com>
2025-02-18 01:52:22 +1100

    update

--- repair-N-player-weak.py
+++ repair-N-player-weak.py
@@ -18,11 +18,11 @@ quantifier = sys.argv[1].replace('.lp', '-quantifier.lp')
 files = outputfile + ' ' + terminal
 weak_winnability(inputfile, outputfile)
 
-#termination_playability_slow(inputfile, terminal)
-#build_quantifier('robot', files, inputfile, quantifier)
-#cmd = f'clingo --output=smodels encoding/repair-qbf-4.lp {inputfile} {files} {quantifier}  {sys.argv[2]} |  python qasp2qbf.py | lp2normal2 | lp2acyc | lp2sat | python qasp2qbf.py --cnf2qdimacs > game-slow.qdimacs'
-#os.system(f"bash -c '{cmd}'")
-
-
-cmd = f'clingo -t 3 --opt-mode=opt,{cost} --restart-on-model encoding/repair-4.lp {inputfile} {outputfile} {sys.argv[2]}'
+termination_playability_slow(inputfile, terminal)
+build_quantifier('robot', files, inputfile, quantifier)
+cmd = f'clingo --output=smodels encoding/repair-qbf-4.lp {inputfile} {files} {quantifier}  {sys.argv[2]} |  python qasp2qbf.py | lp2normal2 | lp2acyc | lp2sat | python qasp2qbf.py --cnf2qdimacs > game-slow.qdimacs'
 os.system(f"bash -c '{cmd}'")
+
+
+#cmd = f'clingo -t 3 --opt-mode=opt,{cost} --restart-on-model encoding/repair-4.lp {inputfile} {outputfile} {sys.argv[2]}'
+#os.system(f"bash -c '{cmd}'")
