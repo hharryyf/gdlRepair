8aae6fd2c828f9d295a2a02d50b5b3044c1e6f95
hharryyf <harryheyifan99@gmail.com>
2025-04-02 22:51:17 +1100

    update

--- repair-N-player-weak-gc.py
+++ repair-N-player-weak-gc.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-if len(sys.argv) != 3 and len(sys.argv) != 4:
-    print('Usage python repair-N-player-weak-gc.py [game-name] [cost bound] [optional:path to UNSAT file]')
+if len(sys.argv) != 4 and len(sys.argv) != 5:
+    print('Usage python repair-N-player-weak-gc.py [game-name] [cost bound] [optimization strategy usc/bb] [optional:path to UNSAT file]')
     exit(1)
     
 game = sys.argv[1]
@@ -15,14 +15,16 @@ else:
 
 unsat = 'encoding/termination-unsat.lp'
 
-if len(sys.argv) == 4:
-    unsat = sys.argv[3]
+if len(sys.argv) == 5:
+    unsat = sys.argv[4]
 
 guess = '{'+ 'encoding/repair-4.lp,encoding/holds.lp,' + f'instances/{game}/bound.lp,instances/{game}/rule.lp,instances/{game}/weak-win.lp,instances/{game}/static.lp' + '}'
 check = '{' + f'{unsat},' + f'instances/{game}/static.lp' + '}'
 
-#cmd = f'time python gc1.py {guess} -C {check} --opt-mode=opt{cost} -t 3 --restart-on-model  --opt-strategy=usc,oll --opt-usc-shrink=inv'
-cmd = f'time python gc1.py {guess} -C {check} --opt-mode=opt{cost} -t 3 --restart-on-model  --opt-strategy=bb,dec'
+if sys.argv[3] == 'usc':
+    cmd = f'time python gc1.py {guess} -C {check} --opt-mode=opt{cost} -t 3 --restart-on-model  --opt-strategy=usc,oll --opt-usc-shrink=inv'
+else:
+    cmd = f'time python gc1.py {guess} -C {check} --opt-mode=opt{cost} -t 3 --restart-on-model  --opt-strategy=bb,dec'
 
 #--opt-strategy=bb,dec
 os.system(f'bash -c "{cmd}"')
